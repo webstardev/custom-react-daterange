ef6ba9c9293bc96707e7e006ca3b1c7a4c3df4fb
whitebearcoder <whitebearcoder@outlook.com>
2021-03-12 01:11:33 +0100

    Add index.css

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import reportWebVitals from "./reportWebVitals";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-daterangepicker/daterangepicker.css";
-import "./index.scss";
+import "./index.css";
 
 import App from "./App";
 
